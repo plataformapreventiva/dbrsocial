7284100f7a3642175c449aeb85146a82f5cf9d45
Ollin Demian <ollin.langle@ciencias.unam.mx>
2018-03-23 19:10:26 +0000

    Add docstring to prev_connect function

--- R/connect.R
+++ R/connect.R
@@ -1,4 +1,10 @@
 prev_connect <- function(){
+    #' This function takes the Postgres environment variables
+    #' to automatically connect to the preventivadb database.
+    #' The connection must be saved into a variable in order to
+    #' use future functions.
+    #' Example:
+    #' con <- prev_connect()
   DBI::dbConnect(RPostgreSQL::PostgreSQL(),
   host     =  Sys.getenv("PGHOST"),
   user     =  Sys.getenv("POSTGRES_USER"),
@@ -6,4 +12,3 @@ prev_connect <- function(){
   port     =  Sys.getenv("PGPORT"),
   dbname   =  Sys.getenv("PGDATABASE"))
 }
-
