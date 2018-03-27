c1a17a8c25eb03011bd05cfd91ffa519bed1c001
Ollin Demian <ollin.langle@ciencias.unam.mx>
2018-03-27 00:28:09 +0000

    Added first documented functions

--- R/connect.R
+++ R/connect.R
@@ -1,14 +1,38 @@
 prev_connect <- function(){
-    #' This function takes the Postgres environment variables
+    #' @title prev_connect
+    #'
+    #' @description This function takes the Postgres environment variables
     #' to automatically connect to the preventivadb database.
     #' The connection must be saved into a variable in order to
     #' use future functions.
-    #' Example:
-    #' con <- prev_connect()
+    #'
+    #' @examples con <- prev_connect()
+    #'
+    #' @param No parameter is needed.
   DBI::dbConnect(RPostgreSQL::PostgreSQL(),
   host     =  Sys.getenv("PGHOST"),
   user     =  Sys.getenv("POSTGRES_USER"),
   password =  Sys.getenv("POSTGRES_PASSWORD"),
   port     =  Sys.getenv("PGPORT"),
   dbname   =  Sys.getenv("PGDATABASE"))
+}
+
+load_table <- function(connection,schema,the_table,the_query="SELECT * FROM %s.%s"){
+    #' @title load_table
+    #'
+    #' @description This function loads a connection to
+    #' a given table from a particular schema in a database
+    #' connection.
+    #'
+    #' @param connection DBI connection. A connection to a database
+    #' must be open and given.
+    #' @param schema variable. A valid schema from a database on the
+    #' connected database.
+    #' @param the_table. An existing table in the given schema.
+    #'
+    #' @param the_dic<-load_table(con,raw,sifode_dic)
+    schema    <- deparse(substitute(schema))
+    the_table <- deparse(substitute(the_table))
+    initial <- RPostgreSQL::dbSendQuery(connection,
+                             sprintf(the_query,schema,the_table))
 }
