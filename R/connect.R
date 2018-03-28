9e6fc01f10d45fdb24ac9b2e903371eca32390a8
Ollin Demian <ollin.langle@ciencias.unam.mx>
2018-03-28 17:24:54 +0000

    move docstings above the function

--- R/connect.R
+++ R/connect.R
@@ -1,14 +1,14 @@
+#' @title prev_connect
+#'
+#' @description This function takes the Postgres environment variables
+#' to automatically connect to the preventivadb database.
+#' The connection must be saved into a variable in order to
+#' use future functions.
+#'
+#' @examples con <- prev_connect()
+#'
+#' @param No parameter is needed.
 prev_connect <- function(){
-    #' @title prev_connect
-    #'
-    #' @description This function takes the Postgres environment variables
-    #' to automatically connect to the preventivadb database.
-    #' The connection must be saved into a variable in order to
-    #' use future functions.
-    #'
-    #' @examples con <- prev_connect()
-    #'
-    #' @param No parameter is needed.
   DBI::dbConnect(RPostgreSQL::PostgreSQL(),
   host     =  Sys.getenv("PGHOST"),
   user     =  Sys.getenv("POSTGRES_USER"),
@@ -17,20 +17,20 @@ prev_connect <- function(){
   dbname   =  Sys.getenv("PGDATABASE"))
 }
 
+#' @title load_table
+#'
+#' @description This function loads a connection to
+#' a given table from a particular schema in a database
+#' connection.
+#'
+#' @param connection DBI connection. A connection to a database
+#' must be open and given.
+#' @param schema variable. A valid schema from a database on the
+#' connected database.
+#' @param the_table. An existing table in the given schema.
+#'
+#' @examples the_dic<-load_table(con,raw,sifode_dic)
 load_table <- function(connection,schema,the_table){
-    #' @title load_table
-    #'
-    #' @description This function loads a connection to
-    #' a given table from a particular schema in a database
-    #' connection.
-    #'
-    #' @param connection DBI connection. A connection to a database
-    #' must be open and given.
-    #' @param schema variable. A valid schema from a database on the
-    #' connected database.
-    #' @param the_table. An existing table in the given schema.
-    #'
-    #' @examples the_dic<-load_table(con,raw,sifode_dic)
     the_query <- "SELECT * FROM %s.%s"
     schema    <- deparse(substitute(schema))
     the_table <- deparse(substitute(the_table))
@@ -38,15 +38,15 @@ load_table <- function(connection,schema,the_table){
                              sprintf(the_query,schema,the_table))
 }
 
+#' @title discon_db
+#'
+#' @description This function disconnects a PostgreSQL
+#' connection.
+#'
+#' @param connection DBI connection. A connection to a database must be open and given.
+#'
+#' @examples con <- prev_connect()
 discon_db <- function(connection){
-    #' @title discon_db
-    #'
-    #' @description This function disconnects a PostgreSQL
-    #' connection.
-    #'
-    #' @param connection DBI connection. A connection to a database must be open and given.
-    #'
-    #' @examples con <- prev_connect()
-    #' discon_db(con)
     RPostgreSQL::dbDisconnect(connection)
 }
+
