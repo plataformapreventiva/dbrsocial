cf4ec112a9f928c9b6466e4192de0e32b27c464d
Ollin Demian <ollin.langle@ciencias.unam.mx>
2018-04-04 21:35:14 +0000

    added clear results and sample table functions

--- R/connect.R
+++ R/connect.R
@@ -1,3 +1,5 @@
+#!/usr/bin/env Rscript
+
 #' @title prev_connect
 #'
 #' @description This function takes the Postgres environment variables
@@ -8,6 +10,7 @@
 #' @examples con <- prev_connect()
 #'
 #' @param No parameter is needed.
+#' @export
 prev_connect <- function(){
   DBI::dbConnect(RPostgreSQL::PostgreSQL(),
   host     =  Sys.getenv("PGHOST"),
@@ -30,6 +33,7 @@ prev_connect <- function(){
 #' @param the_table. An existing table in the given schema.
 #'
 #' @examples the_dic<-load_table(con,raw,sifode_dic)
+#' @export
 load_table <- function(connection,schema,the_table){
     the_query <- "SELECT * FROM %s.%s"
     schema    <- deparse(substitute(schema))
@@ -45,58 +49,46 @@ load_table <- function(connection,schema,the_table){
 #'
 #' @param connection DBI connection. A connection to a database must be open and given.
 #'
-#' @examples con <- prev_connect()
+#' @examples discon_db(con)
+#' @export
 discon_db <- function(connection){
     RPostgreSQL::dbDisconnect(connection)
 }
 
-#' @title sample_table
+#' @title clear_results
 #'
-#' @description Random Sample of any given table.
-#' @param connection DBIConnection object to Postgres predictiva data base, as
-#'   returned by \code{\link[DBI]{dbConnect}}.
-#' @param p Sample size as proportion of the total number of households
-#'   in CUIS, defaults to 0.01.
-#' @param seed numeric value to be used to generate a seeding for the PRNG
-#'   random generator in Postgres backend.
-#' @importFrom magrittr %>%
+#' @description This function clears the results from a previous executed
+#' query.
+#'
+#' @param connection DBI connection. A connection to a database must be open and given.
+#'
+#' @examples clear_results(con)
 #' @export
-cuis_raw_sample <- function(connection, p = 0.01, seed = 1234){
-    domicilios_sample_query <- DBI::dbSendQuery(con, "select * from
-        raw.cuis_historico_domicilios tablesample bernoulli($1) repeatable($2)")
-    DBI::dbBind(domicilios_sample_query, list(p, seed))
-    domicilios_sample <- DBI::dbFetch(domicilios_sample_query)
-    DBI::dbClearResult(domicilios_sample_query)
-    cuis_table <- dplyr::tbl(con, dbplyr::in_schema("raw", "cuis_39_9"))
-    cuis_sample <- cuis_table %>%
-        dplyr::select(-actualizacion_sedesol, -data_date) %>%
-        dplyr::filter(llave_hogar_h %in% domicilios_sample$llave_hogar_h) %>%
-        dplyr::collect()
-    return(cuis_sample)
+clear_results <- function(connection){
+    DBI::dbClearResult(DBI::dbListResults(connection)[[1]])
 }
 
-
 #' @title sample_table
 #'
-#' @description Random Sample of any given table.
+#' @description Random Sample of any given table and schema.
 #' @param connection DBIConnection object to Postgres predictiva data base, as
 #'   returned by \code{\link[DBI]{dbConnect}}.
-#' @param p Sample size as proportion of the total number of households
-#'   in CUIS, defaults to 0.01.
+#' @param p Sample size as proportion of the total number of rows in a
+#' table, defaults to 0.01.
 #' @param seed numeric value to be used to generate a seeding for the PRNG
 #'   random generator in Postgres backend.
 #' @importFrom magrittr %>%
+#'
+#' @examples sample_table(con,0.01,1234,raw,cuis_historico_domicilios)
 #' @export
-cuis_raw_sample <- function(con, p = 0.01, seed = 1234){
-    domicilios_sample_query <- DBI::dbSendQuery(con, "select * from raw.cuis_historico_domicilios tablesample bernoulli($1) repeatable($2)")
-    DBI::dbBind(domicilios_sample_query, list(p, seed))
-    domicilios_sample <- DBI::dbFetch(domicilios_sample_query)
-    DBI::dbClearResult(domicilios_sample_query)
-    cuis_table <- dplyr::tbl(con, dbplyr::in_schema("raw", "cuis_39_9"))
-    cuis_sample <- cuis_table %>%
-        dplyr::select(-actualizacion_sedesol, -data_date) %>%
-        dplyr::filter(llave_hogar_h %in% domicilios_sample$llave_hogar_h) %>%
-        dplyr::collect()
-    return(cuis_sample)
+sample_table <- function(connection, p = 0.01, seed = 1234, schema, the_table){
+    the_table <- deparse(substitute(the_table))
+    schema <-  deparse(substitute(schema))
+    query  <-  "SELECT * FROM %s.%s tablesample bernoulli($1) repeatable($2)"
+    the_query <- sprintf(query,schema,the_table)
+    sample_query <- RPostgreSQL::postgresqlExecStatement(con,the_query,c(p,seed))
+    the_sample <- DBI::dbFetch(sample_query)
+    DBI::dbClearResult(sample_query)
+    return(the_sample)
 }
 
