c87aa42d32ef1a454e86f49e32f80794cfb418a9
Ollin Demian <ollin.langle@ciencias.unam.mx>
2018-04-06 21:14:18 +0000

    Change function table sample function to retrive file

--- R/connect.R
+++ R/connect.R
@@ -1,5 +1,3 @@
-#!/usr/bin/env Rscript
-
 #' @title prev_connect
 #'
 #' @description This function takes the Postgres environment variables
@@ -67,40 +65,3 @@ discon_db <- function(connection){
 clear_results <- function(connection){
     DBI::dbClearResult(DBI::dbListResults(connection)[[1]])
 }
-
-#' @title sample_table
-#'
-#' @description Random Sample of any given table and schema.
-#' @param connection DBIConnection object to Postgres predictiva data base, as
-#'   returned by \code{\link[DBI]{dbConnect}}.
-#' @param p Sample size as proportion of the total number of rows in a
-#' table, defaults to 0.01.
-#' @param seed numeric value to be used to generate a seeding for the PRNG
-#'   random generator in Postgres backend.
-#' @importFrom magrittr %>%
-#'
-#' @param lim int. A limit for SQL lines to return.
-#'
-#' @examples sample_table(con,0.01,1234,raw,cuis_historico_domicilios)
-#' @export
-sample_table <- function(connection, p = 0.01, seed = 1234, schema, the_table, lim=0){
-    the_table <- deparse(substitute(the_table))
-    schema <-  deparse(substitute(schema))
-    if (lim==0){
-    query  <-  "SELECT * FROM %s.%s tablesample bernoulli($1) repeatable($2)"
-    the_query <- sprintf(query,schema,the_table)
-    sample_query <- RPostgreSQL::postgresqlExecStatement(con,the_query,c(p,seed))
-    the_sample <- DBI::dbFetch(sample_query)
-    DBI::dbClearResult(sample_query)
-    return(the_sample)
-    }
-    else{
-    query  <-  "SELECT * FROM %s.%s tablesample bernoulli($1) repeatable($2) limit ($3)"
-    the_query <- sprintf(query,schema,the_table)
-    sample_query <- RPostgreSQL::postgresqlExecStatement(con,the_query,c(p,seed,lim))
-    the_sample <- DBI::dbFetch(sample_query)
-    DBI::dbClearResult(sample_query)
-    return(the_sample)
-    }
-}
-
