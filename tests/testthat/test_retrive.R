793a03ec8a17c0954b1b28d14048d6ffbaad7c9c
Ollin Demian <ollin.langle@ciencias.unam.mx>
2018-04-11 18:04:46 +0000

    Cleaner version of sample function test with dependencies outside the expect_equal evaluation

--- tests/testthat/test_retrive.R
+++ tests/testthat/test_retrive.R
@@ -2,5 +2,7 @@ context("Connection to the database")
 library(dbconnection)
 
 test_that("sample_table gives a random sample from a given table",{
-              expect_equal(sum(dim(sample_table(prev_connect(),0.01,1234,raw,cuis_historico_domicilios))==c(2272,6)),2)
+              con <- prev_connect()
+              table <- sample_table(con,0.01,1234,raw,cuis_historico_domicilios)
+              expect_equal(sum(dim(table)==c(2272,6)),2)
 })
