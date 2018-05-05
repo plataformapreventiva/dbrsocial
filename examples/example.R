878143d8a5bd2f74db1d1820a21f92645995f247
Ollin Demian <ollin.langle@ciencias.unam.mx>
2018-05-05 00:40:28 +0000

    connection to Athena and new example

--- examples/example.R
+++ examples/example.R
@@ -9,3 +9,8 @@ domicilios_sample_query <- sample_table(con,0.01,1234,raw,cuis_historico_domicil
 cuis_sample <- large_table(con,raw,cuis_39_9) %>%
     dplyr::filter(llave_hogar_h %in% domicilios_sample_query$llave_hogar_h) %>%
     dplyr::collect()
+
+e_viv <- large_table(con,raw,enigh_viviendas) %>%
+    dplyr::collect() %>%
+    dplyr::rowwise()%>%
+    dplyr::mutate(hola=mean(c(pileta,renta)))
