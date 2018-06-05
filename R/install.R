d6e4bb8f75403743551740fe7cc82d85209486cf
Ollin Demian <ollin.langle@ciencias.unam.mx>
2018-06-05 23:44:58 +0000

    install from branch bugfix

--- R/install.R
+++ R/install.R
@@ -1,4 +1,4 @@
 #### Este archivo sólo es para guardar la forma en la que sebe instalar cierta
 ###rama.
 
-devtools::install_github("plataformapreventiva/dbconnection", ref = "develop",auth_token=Sys.getenv("GITHUB_PATH"), build_vignettes=TRUE)
+devtools::install_github("plataformapreventiva/dbrsocial", ref = "develop",auth_token=Sys.getenv("GITHUB_PATH"), build_vignettes=TRUE)
