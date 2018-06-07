9966c177853e97deb6c11d6e0a1c5e7b49b02e8a
Ollin Demian <ollin.langle@ciencias.unam.mx>
2018-06-07 20:38:04 +0000

    a bit more examples

--- R/install.R
+++ R/install.R
@@ -1,4 +1,5 @@
 #### Este archivo sólo es para guardar la forma en la que sebe instalar cierta
 ###rama.
 
+dotenv::load_dot_env("../.env")
 devtools::install_github("plataformapreventiva/dbrsocial", ref = "develop",auth_token=Sys.getenv("GITHUB_PATH"), build_vignettes=TRUE)
--- examples/example.R
+++ examples/example.R
@@ -2,6 +2,9 @@
 
 library(dbrsocial)
 library(magrittr)
+library(tidyverse)
+
+dotenv::load_dot_env("../.env")
 
 con <- prev_connect()
 
@@ -11,8 +14,27 @@ cuis_sample <- large_table(con,raw,cuis_39_9) %>%
     join_tables(llave_hogar_h, domicilios_sample_query,llave_hogar_h) %>%
     retrive_result()
 
+clear_results(con)
+
+discon_db(con)
 
 e_viv <- large_table(con,raw,enigh_viviendas) %>%
     dplyr::collect() %>%
     dplyr::rowwise()%>%
     dplyr::mutate(alberca=mean(c(pileta,renta)))
+
+
+
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+
+con <- pub_connect(s3dir = Sys.getenv("S3_DIR"), schema = Sys.getenv("SCHEMA"))
+los_queries <- query_dic()
+query <- "SELECT *
+              FROM athena_pub.pub_nominal
+              LIMIT 6;"
+c(examp,los_queries) := load_or_run(con,query,los_queries)
+examp
