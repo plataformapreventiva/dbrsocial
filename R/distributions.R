97a6af12c782cbf996a718b899a2218c725846e4
Ollin Demian <ollin.langle@ciencias.unam.mx>
2018-07-26 21:41:35 +0000

    modifiable columns

--- R/distributions.R
+++ R/distributions.R
@@ -10,8 +10,8 @@
 #' @examples options <- "WHERE anio=2017 AND cast(newid as integer) < 500000000 AND cveent='29' AND cddependencia='20' GROUP BY cdbeneficio, newid"
 #' @examples c(dinero_programa, all_values) <- box_payment(con, the_queries, options=options)
 #' @export
-box_payment <- function(connection,dict,options=""){
-    the_query1 <- "WITH los_montos AS (SELECT numespago, cdbeneficio, newid, sum(nuimpmonetario) as monto FROM athena_pub.pub_public "
+box_payment <- function(connection,dict,columns="numespago, cdbeneficio, newid, sum(nuimpmonetario) as monto FROM athena_pub.pub_public",options=""){
+    the_query1 <- "WITH los_montos AS (SELECT "
     the_query2 <- "),los_valores AS (SELECT cdbeneficio, avg(monto) as media, approx_percentile(monto,0.25) as q1, approx_percentile(monto,0.5) as q2, approx_percentile(monto,0.75) as q3,stddev(monto) as std, array_agg(monto) as valores FROM los_montos GROUP BY cdbeneficio),
 
               los_rangos AS (SELECT cdbeneficio, media, q1, q2, q3,  std, abs(q3)-abs(q1) as IQR, filter(valores, x -> x IS NOT NULL) as nonull FROM los_valores)
@@ -21,7 +21,7 @@ box_payment <- function(connection,dict,options=""){
               array_min(array_intersect(filter(nonull,x -> x > q3),filter(nonull,x -> x < q3+1.5*IQR))) as max,
               array_max(array_intersect(filter(nonull,x -> x < q1),filter(nonull,x -> x > q1-1.5*IQR))) as min
               FROM los_rangos"
-    query <- paste0(the_query1,options,the_query2)
+    query <- paste0(the_query1,columns," ",options,the_query2)
     c(the_df,dict) := load_or_run(connection,query,dict)
     catalogo_beneficios <- csv_s3()
     colnames(catalogo_beneficios) <- c("cdbeneficio","nbbeneficio")
